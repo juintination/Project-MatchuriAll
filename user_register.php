61b9eaef89613d14404116380869f499db9c914a
DEOKJAE KWON <juintin@mail.hongik.ac.kr>
2023-11-27 22:57:18 +0900

    Fix : Check customer_phone duplication for kiosk login

--- user_register.php
+++ user_register.php
@@ -27,52 +27,63 @@ if (oci_fetch_assoc($emailCheckResult)) {
     // 중복된 이메일이 존재하는 경우
     echo "<script>alert('이미 등록된 이메일 주소입니다. 다른 이메일 주소를 사용해주세요.'); window.history.back();</script>";
 } else {
-    // 새로운 PROFILE 생성
-    $defaultProfilePicPath = 'uploads/profile_default.png';
-    $defaultProfilePic = file_get_contents($defaultProfilePicPath);
-
-    // PROFILE 테이블에 데이터 삽입
-    $insertProfileQuery = "INSERT INTO PROFILE (profile_pic, profile_info, is_admin) VALUES (EMPTY_BLOB(), NULL, 0) RETURNING profile_pic, profile_id INTO :blobData, :profileId";
-    $profileStmt = oci_parse($conn, $insertProfileQuery);
-
-    $blobDescriptor = oci_new_descriptor($conn, OCI_D_LOB);
-    oci_bind_by_name($profileStmt, ':blobData', $blobDescriptor, -1, OCI_B_BLOB);
-    oci_bind_by_name($profileStmt, ':profileId', $profileId, 32);
-
-    oci_execute($profileStmt, OCI_DEFAULT);
-
-    if ($defaultProfilePic) {
-        $blobDescriptor->save($defaultProfilePic);
-    }
-
-    oci_commit($conn);
-
-    oci_free_statement($profileStmt);
-    oci_free_descriptor($blobDescriptor);
-
-    // INSERT 쿼리 실행 (회원 정보)
-    $sqlCustomer = "INSERT INTO CUSTOMER (customer_name, customer_birth, customer_sex, customer_phone, customer_email, customer_pw, customer_point, store_id, profile_id)
-    VALUES (:customerName, TO_DATE(:customerBirth, 'YYYY-MM-DD'), :customerSex, :customerPhone, :customerEmail, :customerPw, 0, :storeID, :profileId)";
-
-    $customerStmt = oci_parse($conn, $sqlCustomer);
-    oci_bind_by_name($customerStmt, ':customerName', $customerName);
-    oci_bind_by_name($customerStmt, ':customerBirth', $customerBirth);
-    oci_bind_by_name($customerStmt, ':customerSex', $customerSex);
-    oci_bind_by_name($customerStmt, ':customerPhone', $customerPhone);
-    oci_bind_by_name($customerStmt, ':customerEmail', $customerEmail);
-    oci_bind_by_name($customerStmt, ':customerPw', $customerPw);
-    oci_bind_by_name($customerStmt, ':storeID', $storeID);
-    oci_bind_by_name($customerStmt, ':profileId', $profileId);
-
-    if (oci_execute($customerStmt, OCI_COMMIT_ON_SUCCESS)) {
-        echo "<script>alert('회원가입이 성공적으로 완료되었습니다.'); window.location = 'index.php';</script>";
+    // 핸드폰 번호 중복 확인
+    $phoneCheckQuery = "SELECT customer_id FROM CUSTOMER WHERE customer_phone = :customerPhone";
+    $phoneCheckResult = oci_parse($conn, $phoneCheckQuery);
+    oci_bind_by_name($phoneCheckResult, ':customerPhone', $customerPhone);
+    oci_execute($phoneCheckResult);
+    if (oci_fetch_assoc($phoneCheckResult)) {
+        // 중복된 핸드폰 번호가 존재하는 경우
+        echo "<script>alert('이미 등록된 핸드폰 번호입니다. 다른 번호를 사용해주세요.'); window.history.back();</script>";
     } else {
-        $err = oci_error($customerStmt);
-        echo "회원가입 오류: " . $err['message'];
-        oci_rollback($conn);
+        // 새로운 PROFILE 생성
+        $defaultProfilePicPath = 'uploads/profile_default.png';
+        $defaultProfilePic = file_get_contents($defaultProfilePicPath);
+    
+        // PROFILE 테이블에 데이터 삽입
+        $insertProfileQuery = "INSERT INTO PROFILE (profile_pic, profile_info, is_admin) VALUES (EMPTY_BLOB(), NULL, 0) RETURNING profile_pic, profile_id INTO :blobData, :profileId";
+        $profileStmt = oci_parse($conn, $insertProfileQuery);
+    
+        $blobDescriptor = oci_new_descriptor($conn, OCI_D_LOB);
+        oci_bind_by_name($profileStmt, ':blobData', $blobDescriptor, -1, OCI_B_BLOB);
+        oci_bind_by_name($profileStmt, ':profileId', $profileId, 32);
+    
+        oci_execute($profileStmt, OCI_DEFAULT);
+    
+        if ($defaultProfilePic) {
+            $blobDescriptor->save($defaultProfilePic);
+        }
+    
+        oci_commit($conn);
+    
+        oci_free_statement($profileStmt);
+        oci_free_descriptor($blobDescriptor);
+    
+        // INSERT 쿼리 실행 (회원 정보)
+        $sqlCustomer = "INSERT INTO CUSTOMER (customer_name, customer_birth, customer_sex, customer_phone, customer_email, customer_pw, customer_point, store_id, profile_id)
+        VALUES (:customerName, TO_DATE(:customerBirth, 'YYYY-MM-DD'), :customerSex, :customerPhone, :customerEmail, :customerPw, 0, :storeID, :profileId)";
+    
+        $customerStmt = oci_parse($conn, $sqlCustomer);
+        oci_bind_by_name($customerStmt, ':customerName', $customerName);
+        oci_bind_by_name($customerStmt, ':customerBirth', $customerBirth);
+        oci_bind_by_name($customerStmt, ':customerSex', $customerSex);
+        oci_bind_by_name($customerStmt, ':customerPhone', $customerPhone);
+        oci_bind_by_name($customerStmt, ':customerEmail', $customerEmail);
+        oci_bind_by_name($customerStmt, ':customerPw', $customerPw);
+        oci_bind_by_name($customerStmt, ':storeID', $storeID);
+        oci_bind_by_name($customerStmt, ':profileId', $profileId);
+    
+        if (oci_execute($customerStmt, OCI_COMMIT_ON_SUCCESS)) {
+            echo "<script>alert('회원가입이 성공적으로 완료되었습니다.'); window.location = 'index.php';</script>";
+        } else {
+            $err = oci_error($customerStmt);
+            echo "회원가입 오류: " . $err['message'];
+            oci_rollback($conn);
+        }
+    
+        oci_free_statement($customerStmt);
     }
 
-    oci_free_statement($customerStmt);
 }
 
 oci_close($conn);
