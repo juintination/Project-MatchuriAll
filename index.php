b38dd8e1b939155d29b2ba0607ad6ad73aaf0b11
DEOKJAE KWON <juintin@mail.hongik.ac.kr>
2023-12-16 19:32:31 +0900

    Fix : Add about us

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
   
   <style>
     /* Apply Nanum Gothic font family to relevant elements */
-    body, p, h3 {
+    body, p, h3, h4 {
       font-family: 'Nanum Gothic', sans-serif;
     }
   </style>
@@ -71,7 +71,36 @@
       <div class="row section-container-spacer">
         <div class="col-xs-12 col-md-12">
           <h2>About Us</h2>
+          <p>매추리알은 실시간으로 무인 가게의 매출 데이터를 추적하고 재고를 관리합니다.</p> 
+          <p>관리자는 언제 어디서나 실시간으로 매출과 재고 현황을 확인할 수 있으며</p>
+          <p>해당 가게의 고객들은 실시간으로 구매 내역과 포인트를 확인할 수 있습니다.</p>
         </div>  
+      </div>
+      <div class="row">
+        
+        <div class="col-xs-12 col-md-3">
+          <img src="css/assets/images/djdj.jpg" alt="" class="reveal img-responsive reveal-content image-center">
+          <h3>권덕재</h3>
+          <h4>B989003</h4>
+        </div>
+       
+        <div class="col-xs-12 col-md-3">
+          <img src="css/assets/images/mhmh.png" alt="" class="reveal img-responsive reveal-content image-center">
+          <h3>박민현</h3>
+          <h4>B989019</h4>
+        </div>
+
+        <div class="col-xs-12 col-md-3">
+          <img src="css/assets/images/jeje.jpg" alt="" class="reveal img-responsive reveal-content image-center">
+          <h3>이정은</h3>
+          <h4>C089048</h4>
+        </div>
+        
+        <div class="col-xs-12 col-md-3">
+          <img src="css/assets/images/lcy.png" alt="" width ="215.5" height="206" class="reveal img-responsive reveal-content image-center">
+          <h3>이채영</h3>
+          <h4>C189053</h4>
+        </div>
       </div>
     </div>
   </div>
